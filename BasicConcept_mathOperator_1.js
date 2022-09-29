7dcc78af4609a8054a5d21402aad158ea9da00f2
nada <nadanazmi3479@gmail.com>
2022-09-29 14:10:17 +0700

    29_09_2022-basicConcept_Nada

---
+++ BasicConcept_mathOperator_1.js
@@ -0,0 +1,32 @@
+/*
+    first operation
+    addition
+*/
+var a = 10
+var b = 10
+var c = a+b
+console.log(c)
+/*
+    first operation
+    substractoin
+*/
+var e = 20
+var f = 5
+var g = e-f
+console.log(g)
+/*
+    first operation
+    multiplication
+*/
+var h = 20
+var i = 5
+var j = h*i
+console.log(j)
+/*
+    first operation
+    division
+*/
+var k = 80
+var l = 8
+var m = k/l
+console.log(m)
---
+++ BasicConcept_mathOperator_2.js
@@ -0,0 +1,8 @@
+var R = Math.sqrt(18)
+console.log(R)
+
+var A = Math.pow(2.2,2)
+console.log(A)
+
+var Z = Math.pow(2.3,2)
+console.log(Z)
