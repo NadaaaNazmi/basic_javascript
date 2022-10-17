df4f3c0ae18f4070c855f6eca587489e7ce0e8af
nada <nadanazmi3479@gmail.com>
2022-10-17 15:05:45 +0700

    17-10-2022_dowhile

---
+++ tugaswhile.js
@@ -0,0 +1,19 @@
+console.log('room number')
+for(n=1;n<=15;n++){
+    if(n==13){
+        continue
+    }
+    console.log(n)
+}
+
+//latian
+console.log('continue')
+var sum = 0;
+for(i=4;i<8;i++){
+    if(i==6){
+        continue;
+    }
+    sum +=i;
+}
+console.log(sum);
+
---
+++ whileloop.js
@@ -0,0 +1,40 @@
+// tiga condition
+var i = 1
+var j = 1
+var k = 1
+console.log('perulangan for')
+for(;i<=5;){
+    console.log(i)
+    i+=1
+}console.log('\n')
+
+//while looping
+console.log('perulangan while')
+while(j=5){
+    console.log(j)
+    j+=1
+}console.log('\n')
+
+//do while looping
+console.log('perulangan do while')
+do{
+    console.log(k)
+    k+=1
+}while(k<=5)
+
+//perulangan dengan break
+for(l=1;l<=8;l++){
+    if(l==2){
+        break
+    }
+    console.log(l)
+}console.log('\n')
+
+//perulangan dengan continue
+for(m=1;m<=5;m++){
+    if(m==2){
+        continue
+    }
+    console.log(m)
+}
+
