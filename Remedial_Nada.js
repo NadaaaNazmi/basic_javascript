1a7a5c60d9d9198f7bd089aa94035346af7cfef2
nada <nadanazmi3479@gmail.com>
2022-10-27 13:16:52 +0700

    27-10-2022_remedial

---
+++ Remedial_Nada.js
@@ -0,0 +1,18 @@
+//for
+for (i=1; i<=5; i++){
+    console.log("Nada");
+}console.log('\n')
+
+//while
+var N = 1
+while(N<=5){
+    console.log("Nazmi")
+    N+=1
+}console.log('\n')
+
+//do while
+var R = 1
+do{
+    console.log("Nurlaila")
+    R+=1
+} while(R<=5)
